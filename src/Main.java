d440b86b06da227bdab56788e0f155ba595d6f05
UlitCars <zashibisf@gmail.com>
2024-02-01 16:14:00 +0300

    Home Works

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,68 @@
+import java.util.SortedMap;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет Мир!!!");
         System.out.println("Hello world!!!");
+
+        //Home Work 1
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Home Work 2
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Home Work 3
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Home Work 4
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        //Home Work 5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        //Home Work 6
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var totalWeight = boxer1 + boxer2;
+        System.out.println(totalWeight);
+        var difference = boxer1 - boxer2;
+        System.out.println(difference);
+
+        //Home Work 7
+        var differenceSubtraction = boxer2 - boxer1;
+        System.out.println(differenceSubtraction);
+        var differenceDivision = boxer2 % boxer1;
+        System.out.println(differenceDivision);
+
     }
 }
